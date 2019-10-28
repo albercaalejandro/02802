7c2ba7ef1a2f26d25e0bd8e07f9fa5b776722100
Xtek <Xtek@Pc-Nasa>
2019-10-28 17:47:26 +0100

    :metal: Commit resubido

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package numeroprimitva;
+package org.japo.java.main;
 
 import java.util.Scanner;
 
